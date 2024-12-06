bf4d3402ab6a3e71c6a45cc0bd14a5b84361b949
Shreee11 <shrikantbhandare11@gmail.com>
2024-12-06 18:14:59 +0530

    feat: user api curd opration created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+    public function store(Request $request)
+    {
+       
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'dob' => 'required|date',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'dob' => date($request->dob),
+        ]);
+        return response()->json($user, 201);
+    }
+
+    public function show($id)
+    {
+        return response()->json(User::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+}
+
